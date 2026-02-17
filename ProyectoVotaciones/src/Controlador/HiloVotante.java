57c2421acae3b7fa40798d5e9fd99cde9fb3db05
davidherreros <deiivih02@gmail.com>
2026-02-17 17:09:49 +0100

    Hilo creado y añadido el metodo de insertarvoto de la bbdd

---
+++ ProyectoVotaciones/src/Controlador/HiloVotante.java
@@ -0,0 +1,72 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.util.Random;
+
+public class HiloVotante extends Thread {
+
+    private String comunidad;
+    private String rangoEdad;
+    private BaseDeDatos bd;
+    private Connection con;
+
+    public HiloVotante(String comunidad, String rangoEdad, BaseDeDatos bd, Connection con) {
+        this.comunidad = comunidad;
+        this.rangoEdad = rangoEdad;
+        this.bd = bd;
+        this.con = con;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            Random random = new Random();
+            int numero = random.nextInt(101);
+
+            String partido = calcularPartido(numero);
+
+            bd.insertarVoto(con, comunidad, rangoEdad, partido);
+
+            Thread.sleep(100);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String calcularPartido(int numero) {
+
+        if (rangoEdad.equals("18_25")) {
+            if (numero <= 30) return "PP";
+            else if (numero <= 50) return "PSOE";
+            else if (numero <= 70) return "VOX";
+            else return "SALF";
+        }
+
+        if (rangoEdad.equals("26_40")) {
+            if (numero <= 20) return "PP";
+            else if (numero <= 55) return "PSOE";
+            else if (numero <= 85) return "VOX";
+            else return "SALF";
+        }
+
+        if (rangoEdad.equals("41_65")) {
+            if (numero <= 10) return "PP";
+            else if (numero <= 55) return "PSOE";
+            else if (numero <= 90) return "VOX";
+            else return "SALF";
+        }
+
+        if (rangoEdad.equals("MAS_66")) {
+            if (numero <= 25) return "PP";
+            else if (numero <= 60) return "PSOE";
+            else if (numero <= 95) return "VOX";
+            else return "SALF";
+        }
+
+        return "PP";
+    }
+
+}
